1d20b1f1d4018dcff08504932beb09133b1441dc
toan <nguyencongtoan14051997@gmail.com>
2020-10-12 03:16:04 +0700

    Home Admin

---
+++ SportVueClient/src/views/admin/HomeAdmin.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <v-row>
+      <v-col
+        ><div class="border1" style="background-color: green">
+          <h3 class="text-center">{{ this.tournament.length }} TOURNAMENT</h3>
+        </div></v-col
+      >
+      <v-col
+        ><div class="border1" style="background-color: blue">
+          <h3 class="text-center">{{ this.schedule.length }} SCHEDULE</h3>
+        </div></v-col
+      >
+      <v-col
+        ><div class="border1" style="background-color: red">
+          <h3 class="text-center">20 TEAM</h3>
+        </div></v-col
+      >
+    </v-row>
+    <h2>Notification</h2>
+    <!-- <div v-for="(item,i) in schedule" :key="i">
+        <div v-if="item.video==null &&item.status==2"> -->
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Time</th>
+            <th class="text-left">Team</th>
+            <th class="text-left"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, i) in schedule" :key="i">
+            <template v-if="item.video == null && item.status == 2">
+              <td>{{ item.title }}</td>
+              <td>{{ item.timeStart }} ->{{ item.timeEnd }}</td>
+              <td>
+                {{ item.team[0].nameTeam }}-<b>vs</b>-{{
+                  item.team[1].nameTeam
+                }}
+              </td>
+              <td><a :href="$router.resolve({ path: '/DetailSchedule/' + item.idSchedule }).href">Update Now </a></td>
+            </template>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tournament: "",
+      schedule: "",
+    };
+  },
+  created() {
+    this.$store.dispatch("tournament/getAll").then((response) => {
+      this.tournament = response.data;
+    });
+    this.$store.dispatch("schedule/getAll").then((response) => {
+      this.schedule = response.data;
+      console.log(this.schedule);
+    });
+  },
+};
+</script>
+<style>
+.border1 {
+  border-radius: 30px;
+  background-color: green;
+  color: white;
+  width: 400px;
+  height: 100px;
+  padding: 20px;
+}
+</style>
---
+++ SportVueClient/src/views/web/contents/BaskestBall/LayoutBaskestBall.vue
@@ -0,0 +1,225 @@
+<template>
+  <v-card>
+    <v-tabs v-model="tab" class="p-1" fixed-tabs>
+      <v-tabs-slider></v-tabs-slider>
+      <v-tab>Các giải đấu</v-tab>
+      <v-tab>Đang diễn ra</v-tab>
+      <v-tab>Kết thúc</v-tab>
+      <v-tab>Sắp diễn ra</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab" style="margin-top: 50px">
+      <v-tab-item>
+        <v-expansion-panels v-model="panel" multiple>
+          <v-expansion-panel v-for="(tournament, i) in tournaments" :key="i">
+            <v-expansion-panel-header class="header"
+              ><h5>{{ tournament.nameTour }}</h5></v-expansion-panel-header
+            >
+            <v-expansion-panel-content>
+              <v-list dense>
+                <v-list-item-group color="primary">
+                  <v-list-item
+                    v-for="(item, index) in tournament.schedule"
+                    :key="index"
+                  >
+                    <v-list-item-content>
+                      <v-row
+                        v-b-popover.hover.right="'Kích vào để xem chi tiết'"
+                        @click="detail(item)"
+                        v-model="open"
+                      >
+                        <v-col cols="12" sm="4">Đang diễn ra</v-col>
+                        <v-col cols="12" sm="8">
+                          <v-row>
+                            <v-col> {{ item.team[0].nameTeam }}</v-col>
+                            <v-col
+                              >{{ item.status == 2 ? item.scoreTeam1 : "?" }}-
+                              {{
+                                item.status == 2 ? item.scoreTeam2 : "?"
+                              }}</v-col
+                            >
+                            <v-col>{{ item.team[1].nameTeam }}</v-col>
+                          </v-row></v-col
+                        >
+                      </v-row>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-expansion-panel-content>
+          </v-expansion-panel> </v-expansion-panels
+      ></v-tab-item>
+      <v-tab-item>
+        <v-expansion-panels v-model="panel" multiple>
+          <v-expansion-panel v-for="(tournament, i) in tournamentsLive" :key="i">
+            <v-expansion-panel-header class="header"
+              ><h5>{{ tournament.nameTour }}</h5></v-expansion-panel-header
+            >
+            <v-expansion-panel-content>
+              <v-list dense>
+                <v-list-item-group color="primary">
+                  <v-list-item
+                    v-for="(item, index) in tournament.schedule"
+                    :key="index"
+                  >
+                    <v-list-item-content>
+                      <v-row
+                        v-b-popover.hover.right="'Kích vào để xem chi tiết'"
+                        @click="detail(item)"
+                        v-model="open"
+                      >
+                        <v-col cols="12" sm="4">Đang diễn ra</v-col>
+                        <v-col cols="12" sm="8">
+                          <v-row>
+                            <v-col> {{ item.team[0].nameTeam }}</v-col>
+                            <v-col
+                              >{{ item.status == 2 ? item.scoreTeam1 : "?" }}-
+                              {{
+                                item.status == 2 ? item.scoreTeam2 : "?"
+                              }}</v-col
+                            >
+                            <v-col>{{ item.team[1].nameTeam }}</v-col>
+                          </v-row></v-col
+                        >
+                      </v-row>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-expansion-panel-content>
+          </v-expansion-panel> </v-expansion-panels
+      ></v-tab-item>
+      <v-tab-item> <v-expansion-panels v-model="panel" multiple>
+          <v-expansion-panel v-for="(tournament, i) in tournamentsEnd" :key="i">
+            <v-expansion-panel-header class="header"
+              ><h5>{{ tournament.nameTour }}</h5></v-expansion-panel-header
+            >
+            <v-expansion-panel-content>
+              <v-list dense>
+                <v-list-item-group color="primary">
+                  <v-list-item
+                    v-for="(item, index) in tournament.schedule"
+                    :key="index"
+                  >
+                    <v-list-item-content>
+                      <v-row
+                        v-b-popover.hover.right="'Kích vào để xem chi tiết'"
+                        @click="detail(item)"
+                        v-model="open"
+                      >
+                        <v-col cols="12" sm="4">Đang diễn ra</v-col>
+                        <v-col cols="12" sm="8">
+                          <v-row>
+                            <v-col> {{ item.team[0].nameTeam }}</v-col>
+                            <v-col
+                              >{{ item.status == 2 ? item.scoreTeam1 : "?" }}-
+                              {{
+                                item.status == 2 ? item.scoreTeam2 : "?"
+                              }}</v-col
+                            >
+                            <v-col>{{ item.team[1].nameTeam }}</v-col>
+                          </v-row></v-col
+                        >
+                      </v-row>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-expansion-panel-content>
+          </v-expansion-panel> </v-expansion-panels
+      ></v-tab-item>
+       <v-tab-item>
+        <v-expansion-panels v-model="panel" multiple>
+          <v-expansion-panel v-for="(tournament, i) in tournamentsUpcoming" :key="i">
+            <v-expansion-panel-header class="header"
+              ><h5>{{ tournament.nameTour }}</h5></v-expansion-panel-header
+            >
+            <v-expansion-panel-content>
+              <v-list dense>
+                <v-list-item-group color="primary">
+                  <v-list-item
+                    v-for="(item, index) in tournament.schedule"
+                    :key="index"
+                  >
+                    <v-list-item-content>
+                      <v-row
+                        v-b-popover.hover.right="'Kích vào để xem chi tiết'"
+                        @click="detail(item)"
+                        v-model="open"
+                      >
+                        <v-col cols="12" sm="4">Đang diễn ra</v-col>
+                        <v-col cols="12" sm="8">
+                          <v-row>
+                            <v-col> {{ item.team[0].nameTeam }}</v-col>
+                            <v-col
+                              >{{ item.status == 2 ? item.scoreTeam1 : "?" }}-
+                              {{
+                                item.status == 2 ? item.scoreTeam2 : "?"
+                              }}</v-col
+                            >
+                            <v-col>{{ item.team[1].nameTeam }}</v-col>
+                          </v-row></v-col
+                        >
+                      </v-row>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-expansion-panel-content>
+          </v-expansion-panel> </v-expansion-panels
+      ></v-tab-item>
+    </v-tabs-items>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tab: null,
+      tournaments: "",
+      panel: [0, 1, 2, 3, 4, 5],
+      tournamentsUpcoming:'',
+      tournamentsLive:'',
+      tournamentsEnd:'',
+    };
+  },
+  created() {
+    this.$store
+      .dispatch("tournament/getByType", "BaskestBall")
+      .then((response) => {
+        this.tournaments = response.data;
+      });
+    this.$store
+      .dispatch("tournament/getByStatus", { status: "0", type: "BaskestBall" })
+      .then((response) => {
+        this.tournamentsUpcoming = response.data;
+      });
+    this.$store
+      .dispatch("tournament/getByStatus", { status: "1", type: "BaskestBall" })
+      .then((response) => {
+        this.tournamentsLive = response.data;
+      });
+    this.$store
+      .dispatch("tournament/getByStatus", { status: "2", type: "BaskestBall" })
+      .then((response) => {
+        this.tournamentsEnd = response.data;
+      });
+  },
+   methods: {
+    detail(data) {
+      this.detailScore = data;
+      this.open = true;
+      var myWindow = window.open(
+        "http://localhost:8080/soccer/detail/" + data.idSchedule,
+        "myWindow",
+        "width=600px,height=600"
+      );
+    },
+  },
+};
+</script>
+<style>
+.header {
+  color: red;
+  font-weight: bold;
+}
+</style>
